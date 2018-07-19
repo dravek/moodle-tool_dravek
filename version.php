dfd4d48b67eb02530c482e18858f7e79d03e08e9
David Matamoros <davidmc@moodle.com>
2018-07-19 16:29:11 +0200

    First commit

---
+++ version.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package   tool_dravek
+ * @copyright 2018, David <davidmc@moodle.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component    = 'tool_dravek';
+$plugin->version      = 2018071900;
+$plugin->requires     = 2015111600;
+$plugin->release      = '1.0';
+$plugin->maturity     = MATURITY_STABLE;
