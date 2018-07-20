42e56452ba9b2ca53667e6af9b48d2b3e7bd2709
David Matamoros <davidmc@moodle.com>
2018-07-20 08:49:24 +0200

    1.2 using html_writer

--- index.php
+++ index.php
@@ -6,4 +6,11 @@ $PAGE->set_url($url);
 $PAGE->set_pagelayout('report');
 $PAGE->set_title('Hello to the todo list');
 $PAGE->set_heading(get_string('pluginname', 'tool_dravek'));
-echo get_string('helloworld', 'tool_dravek');
+
+
+
+echo $OUTPUT->header();
+
+echo html_writer::span(get_string('helloworld', 'tool_dravek', ['id' => 1]));
+
+echo $OUTPUT->footer();
--- lang/en/tool_dravek.php
+++ lang/en/tool_dravek.php
@@ -1,3 +1,3 @@
 <?php
 $string['pluginname'] = 'My first Moodle plugin';
-$string['helloworld'] = 'Hello World';
+$string['helloworld'] = 'Hello World {$a->id}';
