394a0920584c2542fcde4f4b690879edfc542099
David Matamoros <davidmc@moodle.com>
2018-07-20 13:56:55 +0200

    travis.yml

--- index.php
+++ index.php
@@ -1,9 +1,26 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
 require_once(__DIR__ . '/../../../config.php');
+
+global $DB;
 
 $courseid = required_param('id', PARAM_INT);
 
-$url = new moodle_url('/admin/tool/dravek/index.php',array('id'=>$courseid));
+$url = new moodle_url('/admin/tool/dravek/index.php',array('id' => $courseid));
 $PAGE->set_context(context_system::instance());
 $PAGE->set_url($url);
 $PAGE->set_pagelayout('report');
@@ -11,13 +28,11 @@ $PAGE->set_title('Hello to the todo list');
 $PAGE->set_heading(get_string('pluginname', 'tool_dravek'));
 
 $PAGE->navbar->add(get_string('plugin'), new moodle_url($url));
-//$coursenode = $PAGE->navigation->find($courseid, navigation_node::TYPE_COURSE);
-//$thingnode = $coursenode->add('Name of thing', new moodle_url('/a/link/if/you/want/one.php'));
-//$thingnode->make_active();
 
+$users = $DB->get_record_sql('SELECT count(*) as total FROM {user}');
+var_dump($users);
 
 echo $OUTPUT->header();
-
 echo html_writer::span(get_string('helloworld', 'tool_dravek', ['id' => $courseid]));
-
+echo html_writer::span(get_string('totalusers', 'tool_dravek', ['total' => $users->total]));
 echo $OUTPUT->footer();
--- lib.php
+++ lib.php
@@ -1,15 +1,28 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 defined('MOODLE_INTERNAL') || die;
 
-function tool_dravek_extend_navigation_course($navigation, $course, $context)
-{
+function tool_dravek_extend_navigation_course($navigation, $course, $context){
     global $PAGE;
     $courseid = $PAGE->course->id;
 
     $navigation->add(
         get_string('pluginname', 'tool_dravek'),
-        new moodle_url('/admin/tool/dravek/index.php?', array('id'=>$courseid)),
+        new moodle_url('/admin/tool/dravek/index.php?', array('id' => $courseid)),
         navigation_node::TYPE_SETTING,
         get_string('pluginname', 'tool_dravek'),
         'todolist');
