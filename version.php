6fb357a9fa02913516634578e7826dc2a9380fdf
David Matamoros <davidmc@moodle.com>
2018-07-25 17:12:44 +0200

    1.8 version.php

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'tool_dravek';
-$plugin->version      = 2018072402;
+$plugin->version      = 2018072500;
 $plugin->requires     = 2015111600;
-$plugin->release      = '1.6';
+$plugin->release      = '1.8';
 $plugin->maturity     = MATURITY_STABLE;
