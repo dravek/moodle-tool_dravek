393d23811c6fde1dab1032c377ed4a6151cfd65a
David Matamoros <davidmc@moodle.com>
2018-08-08 10:23:57 +0200

    v2.5 WS and Ajax - travis workaround for behat

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'tool_dravek';
-$plugin->version      = 2018080700;
+$plugin->version      = 2018080800;
 $plugin->requires     = 2015111600;
 $plugin->release      = '2.5';
 $plugin->maturity     = MATURITY_STABLE;
